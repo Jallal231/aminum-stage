24480b976bc428b877b2a3ac1cdafed4e3db6fcc
Jallal231 <neaglebtw@gmail.com>
2022-04-19 16:25:37 +0200

    Debut table multiplication

--- petit-bac/petitbac.js
+++ petit-bac/petitbac.js
@@ -1,3 +1,118 @@
-console.log("test"); 
-let id_generate = 0;
-let liste = document.getElementById("les_sujets");
+console.log("Début du petit bac");
+
+let petitbac = document.getElementById("tableSaisie");
+
+let btnAjouter = document.getElementById("bouton_ajouter");
+btnAjouter.addEventListener("click", ajouter);
+
+let btnClean = document.getElementById("bouton_clean");
+btnClean.addEventListener("click", clean);
+
+let btnGenerer = document.getElementById("bouton_generer");
+btnGenerer.addEventListener("click", genererLETTRE);
+
+let btnRenitialiser = document.getElementById("bouton_renitialiser");
+btnRenitialiser.addEventListener("click", renitialiser);
+
+
+
+// DEBUT DE MA FONCTION  AJOUTER
+
+
+
+function ajouter() {
+
+
+    let tr = document.createElement("tr");
+    tr.classList.add("rajout_ligne");
+    
+    console.log(petitbac);
+    ajouterCellule("Lettre", tr);
+    ajouterCellule("Nom", tr);  
+    ajouterCellule("Prenom", tr);
+    ajouterCellule("Objet", tr);
+    ajouterCellule("Animaux", tr);
+    ajouterCellule("Métiers", tr);
+    ajouterCellule("Pays", tr);
+    ajouterCellule("Fruits", tr);
+    petitbac.appendChild(tr);
+}
+
+
+function ajouterCellule(mots, tr) {
+
+    let cellule = document.createElement("td"); 
+    let saisie = document.getElementById(mots).value;
+    cellule.innerText = saisie;
+    tr.appendChild(cellule);  
+
+}
+ 
+
+
+
+// DEBUT DE MA FONCTION CLEAN 
+
+ 
+function clean() {
+
+    nettoyerCellule("Lettre");
+    nettoyerCellule("Nom");
+    nettoyerCellule("Prenom");
+    nettoyerCellule("Objet"); 
+    nettoyerCellule("Animaux"); 
+    nettoyerCellule("Métiers");
+    nettoyerCellule("Pays"); 
+    nettoyerCellule("Fruits");
+
+}
+ 
+ 
+function nettoyerCellule(mots) {
+
+    let Cellule = document.getElementById(mots).value = " ";
+    console.log(Cellule);  
+    // celule.innerText =" "; 
+    console.log(Cellule); 
+
+
+
+}
+
+ 
+// DEBUT DE MA FONCTION GENERER
+ 
+ 
+function genererLETTRE() {
+    let min = 1;
+    let max = 26;
+    let chiffre = Math.round(Math.random() * (max - min) + min);
+    console.log(chiffre);
+    let lettre = String.fromCharCode(64 + chiffre);
+
+
+    // console.log(String.fromCharCode(64 + min));
+    // console.log(String.fromCharCode(64 + max)); 
+
+    let maLettre = document.getElementById("Lettre_de_jeu");
+    console.log(maLettre);
+    maLettre.innerText = lettre;
+}
+
+ 
+ // DEBUT DE MA FONCTION RENITIALISER 
+  
+
+function renitialiser() {
+    let liste_lignes = document.getElementsByClassName('rajout_ligne')
+         // console.log(rajout_ligne);
+    //let nb_elements = liste_lignes.length
+         for (let  element in liste_lignes){
+
+             
+         liste_lignes[element].innerHTML=""
+
+
+    }
+}
+
