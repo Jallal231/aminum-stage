8dcf2d98c4a1b98e0b365eaad0526912ed18d55e
Jallal231 <neaglebtw@gmail.com>
2022-04-20 16:43:53 +0200

    Update 20/04

---
+++ table-multiplication/table.js
@@ -0,0 +1,37 @@
+console.log("Début table multiplication");
+  
+let  checkcalcule = document.getElementById("tableauveirf");
+
+let btnInit = document.getElementById("BoutonInit");
+BoutonInit.addEventListener("click", init); 
+ 
+let btnCheckInit = document.getElementById("BoutonCheckInit"); 
+BoutonCheckInit.addEventListener("click" , checkinit);
+ 
+let btnCheck = document.getElementById("BoutonCheck");
+BoutonCheck.addEventListener("click" , check); 
+ 
+
+
+ 
+function init () { 
+     
+    InitCalcule("calcule1" , "calcule2"); 
+
+} 
+
+ 
+function InitCalcule("cal1" , "calcule2"); { 
+     
+
+     
+
+} 
+ 
+
+ 
+function checkinit () { 
+
+     checkinit("Calcule1" , "Calcule2");
+     
+}
