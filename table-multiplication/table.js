1a4b603689bab31f3ea74d61619d1d73ecf1b4f0
Jallal231 <neaglebtw@gmail.com>
2022-04-21 13:07:54 +0200

    Update 21/04

--- table-multiplication/table.js
+++ table-multiplication/table.js
@@ -9,23 +9,28 @@ let btnCheckInit = document.getElementById("BoutonCheckInit");
 BoutonCheckInit.addEventListener("click" , checkinit);
  
 let btnCheck = document.getElementById("BoutonCheck");
-BoutonCheck.addEventListener("click" , check); 
+BoutonCheck.addEventListener("click" ,  check); 
  
 
 
  
-function init () { 
-     
-    InitCalcule("calcule1" , "calcule2"); 
 
-} 
 
  
-function InitCalcule("cal1" , "calcule2"); { 
+function init() { 
      
+    let min = 1;
+    let max = 26;
+    let init1 = Math.round(Math.random() * (max - min) + min); 
+    let init2 = Math.round(Math.random() * (max - min) + min);
+    let input1="input1"
+    document.getElementById(input1).value = init1; 
+    document.getElementById("input2").value = init2;
 
-     
-
+    // input1.value = calcule1;
+    // input2.value = calcule2;
+    
+    console.log(init);
 } 
  
 
@@ -35,3 +40,11 @@ function checkinit () {
      checkinit("Calcule1" , "Calcule2");
      
 }
+ 
+ function check () { 
+      
+
+ }
+ 
+console.log("Fin table multiplication");
+ 
